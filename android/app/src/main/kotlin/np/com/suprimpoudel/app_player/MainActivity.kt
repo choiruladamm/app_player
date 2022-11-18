255b95f0f8962db05ce3bf1c8d8005276bd4ab32
choiruladamm <choiruladamm@gmail.com>
2022-11-19 01:35:58 +0700

    app_player

---
+++ android/app/src/main/kotlin/np/com/suprimpoudel/app_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package np.com.suprimpoudel.app_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_player/android/app/src/main/kotlin/app_player/app_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package app_player.app_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
